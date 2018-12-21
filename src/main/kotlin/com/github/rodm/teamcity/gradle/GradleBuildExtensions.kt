c847ee9d44c852b199acabdb15d1b9bd088a6cf2
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2018-12-21 15:54:23 +0000

    Add DSL extension to configure a Gradle init script

---
+++ src/main/kotlin/com/github/rodm/teamcity/gradle/GradleBuildExtensions.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018 Rod MacKenzie.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.rodm.teamcity.gradle
+
+import jetbrains.buildServer.configs.kotlin.v2018_1.BuildFeature
+import jetbrains.buildServer.configs.kotlin.v2018_1.BuildFeatures
+import jetbrains.buildServer.configs.kotlin.v2018_1.ErrorConsumer
+
+class GradleInitScript() : BuildFeature() {
+
+    init {
+        type = "gradle-init-scripts"
+    }
+
+    constructor(init: GradleInitScript.() -> Unit): this() {
+        init()
+    }
+
+    var scriptName by stringParameter("name")
+
+    override fun validate(consumer: ErrorConsumer) {
+        super.validate(consumer)
+        if (scriptName == null && !hasParam("scriptName")) {
+            consumer.consumePropertyError("scriptName", "mandatory 'scriptName' property is not specified")
+        }
+    }
+}
+
+fun BuildFeatures.gradleInitScript(init: GradleInitScript.() -> Unit): GradleInitScript {
+    val result = GradleInitScript(init)
+    feature(result)
+    return result
+}
---
+++ src/test/kotlin/com/github/rodm/teamcity/gradle/GradleBuildExtensionsTest.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2018 Rod MacKenzie.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.rodm.teamcity.gradle
+
+import jetbrains.buildServer.configs.kotlin.v2018_1.BuildFeature
+import jetbrains.buildServer.configs.kotlin.v2018_1.BuildType
+import jetbrains.buildServer.configs.kotlin.v2018_1.Consumer
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class GradleBuildExtensionsTest {
+
+    @Test
+    fun `add Gradle Init Script feature to a build configuration`() {
+        val buildType = BuildType {
+            features {
+                gradleInitScript {}
+            }
+        }
+
+        assertEquals(1, buildType.features.items.size)
+        val feature = buildType.features.items[0]
+        assertEquals("gradle-init-scripts", feature.type)
+    }
+
+    @Test
+    fun `add Gradle Init Script feature with script name`() {
+        val buildType = BuildType {
+            features {
+                gradleInitScript {
+                    scriptName = "init.gradle"
+                }
+            }
+        }
+
+        val feature = buildType.features.items[0]
+        assertEquals("init.gradle", feature.findParam("name"))
+    }
+
+    @Test
+    fun `validating Gradle Init Script feature with script name reports no errors`() {
+        val buildType = BuildType {
+            features {
+                gradleInitScript {
+                    scriptName = "init.gradle"
+                }
+            }
+        }
+
+        val validationConsumer = Consumer()
+        buildType.validate(validationConsumer)
+        assertEquals(0, validationConsumer.errors.size)
+    }
+
+    @Test
+    fun `validating Gradle Init Script feature without script name reports errors`() {
+        val buildType = BuildType {
+            features {
+                gradleInitScript {}
+            }
+        }
+
+        val validationConsumer = Consumer()
+        buildType.validate(validationConsumer)
+        assertEquals(1, validationConsumer.errors.size)
+        assertEquals("build feature [1/1]: mandatory 'scriptName' property is not specified", validationConsumer.errors[0])
+    }
+}
+
+fun BuildFeature.findParam(name: String) : String? {
+    params.forEach { param ->
+        if (param.name == name) return param.value
+    }
+    return null
+}
