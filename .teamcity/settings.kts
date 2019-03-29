26f9711bbe6d697f3a87d57256b480468c946b55
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2019-03-29 20:22:30 +0000

    Update DSL extension library and settings to use 2018.2 API

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,14 +1,14 @@
 
 import com.github.rodm.teamcity.pipeline
-import jetbrains.buildServer.configs.kotlin.v2018_1.CheckoutMode.ON_SERVER
-import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.gradle
-import jetbrains.buildServer.configs.kotlin.v2018_1.project
-import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.VcsTrigger.QuietPeriodMode.USE_DEFAULT
-import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2018_1.vcs.GitVcsRoot
-import jetbrains.buildServer.configs.kotlin.v2018_1.version
+import jetbrains.buildServer.configs.kotlin.v2018_2.CheckoutMode.ON_SERVER
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2018_2.project
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.VcsTrigger.QuietPeriodMode.USE_DEFAULT
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.v2018_2.version
 
-version = "2018.1"
+version = "2018.2"
 
 project {
 
